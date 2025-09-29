5c61f69639d3bf0ae7306dc48ceb9f1c0846a731
Assane Thiao <132925798+assanethiao@users.noreply.github.com>
2025-09-29 15:02:34 +0200

    Create ProjetPi.java

---
+++ ProjetPi.java
@@ -0,0 +1,28 @@
+package serie0;
+
+public class ProjetPi {
+
+	public static void main(String[] args) {
+		
+		double piApprocher = 0; // nombre à approcher
+		int i = 1; 
+		int j = 1; // compteur
+		
+		while(j <= 1000) { // 1000 termes
+			if(j % 2 == 1){
+				piApprocher += 1.0 / i; 
+			} else {
+				piApprocher -= 1.0 / i;
+			}
+			i += 2; 
+			j++;
+		}
+		
+		piApprocher *= 4;
+		
+		System.out.println("Valeur approchée de Pi : " + piApprocher);
+		System.out.println("Valeur de Math.PI     : " + Math.PI);
+
+	}
+
+}
