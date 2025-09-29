4992acbb3601193062df45d77d63d558aff76f62
Assane Thiao <132925798+assanethiao@users.noreply.github.com>
2025-09-29 15:03:05 +0200

    Create ProjetDevin.java

---
+++ ProjetDevin.java
@@ -0,0 +1,47 @@
+package serie0;
+
+import java.util.Scanner;
+
+public class ProjetDevin {
+    public static void main(String[] args) {
+        // Définition des bornes
+        int Min = 0;
+        int Max = 99;
+
+        // Génération d'un nombre aléatoire entre Min et Max
+        int nombreAleatoire = Min + (int) (Math.random() * ((Max - Min) + 1));
+
+        // Scanner pour lire les entrées utilisateur
+        Scanner sc = new Scanner(System.in);
+
+        int essais = 0;
+        int maxEssais = 6;
+        int nombreSaisi = -1;
+
+        System.out.println("Bienvenue au jeu du Devin !");
+        System.out.println("Devinez un nombre entre " + Min + " et " + Max + ".");
+        System.out.println("Vous avez " + maxEssais + " essais maximum.");
+
+        // Boucle de jeu
+        while (essais < maxEssais && nombreSaisi != nombreAleatoire) {
+            System.out.print("Veuillez saisir un nombre : ");
+            nombreSaisi = sc.nextInt();
+            essais++;
+
+            if (nombreSaisi < nombreAleatoire) {
+                System.out.println("Trop petit !");
+            } else if (nombreSaisi > nombreAleatoire) {
+                System.out.println("Trop grand !");
+            } else {
+                System.out.println("Bravo ! Vous avez trouvé le nombre en " + essais + " essais.");
+                break;
+            }
+
+            if (essais == maxEssais && nombreSaisi != nombreAleatoire) {
+                System.out.println("Vous avez atteint le nombre maximum d'essais. Le nombre était : " + nombreAleatoire);
+            }
+        }
+
+        sc.close();
+    }
+}
