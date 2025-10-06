94227cd466c062bb301d4a1ae6cdd1ab9acbaa12
Assane Thiao <132925798+assanethiao@users.noreply.github.com>
2025-10-06 13:50:39 +0200

    Create factorielle.java

---
+++ factorielle.java
@@ -0,0 +1,41 @@
+import java.math.BigInteger;
+public class Factorielle {
+	
+	public int intFactorielle(int n) {
+		if(n==0 || n==1) {
+			return 1;
+		}else {
+			return n * intFactorielle(n-1);
+		}
+		
+	}
+	
+	public double doubleFactorielle(int n) {
+		if(n==0 || n==1) {
+			return 1;
+		}else {
+			return n * intFactorielle(n-1);
+		}
+		
+	}
+	public BigInteger bigIntFactorielle(int n) {
+        if (n == 0 || n == 1) {
+            return BigInteger.ONE; 
+        } else {
+            return BigInteger.valueOf(n).multiply(bigIntFactorielle(n - 1));
+        }
+    }
+}
+ 
+public class FactorielleMain {
+
+	public static void main(String[] args) {
+		Factorielle fact = new Factorielle() ;
+		
+		int [] nombres = {10,13,25,1000} ;
+		// Un exemple d'appel
+		System.out.println("Factorielle de 13 = " +fact.bigIntFactorielle(nombres[1])) ;
+		
+	}
+
+}
